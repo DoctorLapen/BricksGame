2a0b53256bb38eac3bc6b4ccb2d3c4fa3202589b
DoctorLapen <kostia1272@gmail.com>
2020-10-12 19:46:03 +0300

    Add MinoEditorWindow basic

---
+++ Assets/Editor/MinoEditorWindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SuperBricks.Editor
+{
+    public class MinoEditorWindow : EditorWindow
+    {
+        [MenuItem("Window/MinoEditor")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<MinoEditorWindow>();
+            window.titleContent = new GUIContent("MinoEditor");
+            window.maxSize = new Vector2(100,100);
+            window.Show();
+        }
+        private void OnEnable()
+        {
+            var root = rootVisualElement;
+                
+            var minoEditorUXML = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/MinoEditorWindow.uxml");
+            var cell = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/CellMinoInEditor.uxml");
+            minoEditorUXML.CloneTree(root);
+            var grid = root.Query<VisualElement>("MinoGrid").First();
+          
+            for (int i = 0; i < 200; i++)
+            {
+                cell.CloneTree(grid);
+            }
+
+
+
+
+        }
+
+    }
+
+    
+
+}
