dffd710d51730a7dcb294f4b25622d71c3e2253b
DoctorLapen <kostia1272@gmail.com>
2020-10-23 19:13:17 +0300

    CreateFieldBackgroundWindow in Progress

---
+++ Assets/Editor/CreateFieldBackgroundWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SuperBricks.Editor
+{
+    public class CreateFieldBackgroundWindow : EditorWindow
+    {
+        [MenuItem("Window/CreateFieldBackground")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<CreateFieldBackgroundWindow>();
+            window.titleContent = new GUIContent("CreateFieldBackgroundWindow");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            
+        }
+    }
+}
