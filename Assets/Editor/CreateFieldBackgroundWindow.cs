2305d5bc892031651671a6b7c05b8463e470fd51
DoctorLapen <kostia1272@gmail.com>
2020-10-26 22:34:36 +0200

    add Create FieldBackground with CreateBackgroundWindow

--- Assets/Editor/CreateFieldBackgroundWindow.cs
+++ Assets/Editor/CreateFieldBackgroundWindow.cs
@@ -17,6 +17,8 @@ namespace SuperBricks.Editor
         private FloatField _xOffsetField;
         private FloatField _yOffsetField;
         private ObjectField _parentField;
+        private ColorField _backgroundColorField;
+        private ObjectField _backgroundPrefabField;
 
         [MenuItem("Window/CreateFieldBackground")]
         private static void ShowWindow()
@@ -39,7 +41,9 @@ namespace SuperBricks.Editor
             _spawnPointField = root.Query<ObjectField>("StartPointField").First();
             _parentField = root.Query<ObjectField>("ParentField").First();
             _cellColorField = root.Query<ColorField>("CellColorField").First();
+            _backgroundColorField = root.Query<ColorField>("BackgroundColorField").First();
             _cellPrefabField= root.Query<ObjectField>("SpritePrefabField").First();
+            _backgroundPrefabField= root.Query<ObjectField>("BackgroundPrefabField").First();
             button.clicked += CreateBackground;
 
 
@@ -52,10 +56,35 @@ namespace SuperBricks.Editor
             Transform _spawnPoint = (Transform) _spawnPointField.value;
             Transform _parent = (Transform) _parentField.value;
             SpriteRenderer _cellPrefab = (SpriteRenderer) _cellPrefabField.value;
+            SpriteRenderer _backgroundPrefab = (SpriteRenderer)_backgroundPrefabField.value;
             float xOffset = _xOffsetField.value;
             float yOffset = _yOffsetField.value;
             Color cellColor = _cellColorField.value;
+            Color backgroundColor = _backgroundColorField.value;
             Color originCellColor = _cellPrefab.color;
+            
+            _backgroundPrefab.color = backgroundColor;
+            float columns =  _mainGameSettings.ColumnAmount; 
+            float rows =  _mainGameSettings.RowAmount; 
+            float width = _backgroundPrefab.sprite.bounds.size.x;
+            float  height = _backgroundPrefab.sprite.bounds.size.y;
+            float requiredWidth = (_cellPrefab.sprite.bounds.size.x + xOffset) * columns - xOffset;
+            float requiredHeight = (_cellPrefab.sprite.bounds.size.y + yOffset) * rows - yOffset;
+            Vector3 backgroundSpawnPoint = _spawnPoint.localPosition;
+            backgroundSpawnPoint.x -= _cellPrefab.sprite.bounds.size.x / 2f;
+            backgroundSpawnPoint.y += _cellPrefab.sprite.bounds.size.y / 2f;
+            backgroundSpawnPoint.x += requiredWidth / 2f;
+            backgroundSpawnPoint.y -= requiredHeight / 2f;
+
+            
+            float scaleX = requiredWidth / width ;
+            float scaleY = requiredHeight / height;
+            float scaleZ = 1;
+            _backgroundPrefab.transform.localScale = new Vector3(scaleX,scaleY,scaleZ);
+            
+            Instantiate(_backgroundPrefab,backgroundSpawnPoint, Quaternion.identity,_parent);
+            _backgroundPrefab.transform.localScale = Vector3.up;
+            
 
             _cellPrefab.color = cellColor;
             _cellPrefab.name = "BackgroundCell"; 
