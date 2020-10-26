1ec3c37def0a2473ef324e702aa419fd61dd337e
DoctorLapen <kostia1272@gmail.com>
2020-10-26 20:30:22 +0200

    add Create Background cells with CreateBackgroundWindow

--- Assets/Editor/CreateFieldBackgroundWindow.cs
+++ Assets/Editor/CreateFieldBackgroundWindow.cs
@@ -14,6 +14,9 @@ namespace SuperBricks.Editor
         private ObjectField _spawnPointField;
         private ColorField _cellColorField;
         private ObjectField _cellPrefabField;
+        private FloatField _xOffsetField;
+        private FloatField _yOffsetField;
+        private ObjectField _parentField;
 
         [MenuItem("Window/CreateFieldBackground")]
         private static void ShowWindow()
@@ -31,21 +34,46 @@ namespace SuperBricks.Editor
             UXML.CloneTree(root);
             var button =root.Query<Button>("CreateButton").First();
             _mainGameSettingsField = root.Query<ObjectField>("MainGameSettings").First();
-            _spawnPointField = root.Query<ObjectField>("TransformField").First();
+            _xOffsetField = root.Query<FloatField>("X_OffsetField").First();
+            _yOffsetField = root.Query<FloatField>("Y_OffsetField").First();
+            _spawnPointField = root.Query<ObjectField>("StartPointField").First();
+            _parentField = root.Query<ObjectField>("ParentField").First();
             _cellColorField = root.Query<ColorField>("CellColorField").First();
             _cellPrefabField= root.Query<ObjectField>("SpritePrefabField").First();
-            button.clicked += GetAssembleName;
+            button.clicked += CreateBackground;
 
 
 
         }
 
-        private void GetAssembleName()
+        private void CreateBackground()
         {
             IMainGameSettings _mainGameSettings = (MainGameSettings)_mainGameSettingsField.value;
-            Transform spawnPoint = (Transform) _spawnPointField.value;
-            
-            Debug.Log(_mainGameSettings.RowAmount);
+            Transform _spawnPoint = (Transform) _spawnPointField.value;
+            Transform _parent = (Transform) _parentField.value;
+            SpriteRenderer _cellPrefab = (SpriteRenderer) _cellPrefabField.value;
+            float xOffset = _xOffsetField.value;
+            float yOffset = _yOffsetField.value;
+            Color cellColor = _cellColorField.value;
+            Color originCellColor = _cellPrefab.color;
+
+            _cellPrefab.color = cellColor;
+            _cellPrefab.name = "BackgroundCell"; 
+            Vector3 spawnPosition = _spawnPoint.localPosition;
+            for (int row = 0; row < _mainGameSettings.RowAmount; row++)
+            {
+                for (int column = 0; column < _mainGameSettings.ColumnAmount; column++)
+                {
+                    Instantiate(_cellPrefab, spawnPosition, Quaternion.identity,_parent);
+                    spawnPosition.x += xOffset + _cellPrefab.sprite.bounds.size.x;
+                }
+
+                spawnPosition.x = _spawnPoint.localPosition.x;
+                spawnPosition.y -= yOffset + _cellPrefab.sprite.bounds.size.y;
+            }
+
+            _cellPrefab.color = originCellColor;
+
         }
         
 
