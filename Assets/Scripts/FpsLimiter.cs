01ddc35992887341badf2ab8dbfe65e144b94b8d
DoctorLapen <kostia1272@gmail.com>
2020-12-02 14:20:44 +0200

    Add FpsLimiter

---
+++ Assets/Scripts/FpsLimiter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace SuperBricks
+{
+    public class FpsLimiter : MonoBehaviour 
+    {
+        [SerializeField]
+        private int _targetFrameRate;
+ 
+        private void Start()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = _targetFrameRate;
+        }
+    }
+}
