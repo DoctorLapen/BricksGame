a28cfae6f61974e4ea5e732d59ca0e5c20ce7f4c
DoctorLapen <kostia1272@gmail.com>
2020-12-06 21:09:58 +0200

    add pauseMenu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SuperBricks
+{
+    public class PauseMenu:Menu
+    {
+        [SerializeField]
+        private RectTransform _menuPanel;
+
+        
+        public void  StartPause()
+        {
+            Pause.StopTime();
+            _menuPanel.gameObject.SetActive(true);
+        }
+
+        public void ContinueGame()
+        {
+            _menuPanel.gameObject.SetActive(false);
+            Pause.StartTime();
+        }
+    }
+}
