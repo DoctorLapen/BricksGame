7b15bc1686a835b96148de90dfe616c6b0b57c5b
DoctorLapen <kostia1272@gmail.com>
2020-12-06 21:55:40 +0200

    Fix ui menu right visibility

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -6,18 +6,23 @@ namespace SuperBricks
     {
         [SerializeField]
         private RectTransform _menuPanel;
+        [SerializeField]
+        private RectTransform _pauseButton;
 
         
         public void  StartPause()
         {
             Pause.StopTime();
+            _pauseButton.gameObject.SetActive(false);
             _menuPanel.gameObject.SetActive(true);
         }
 
         public void ContinueGame()
         {
             _menuPanel.gameObject.SetActive(false);
+            _pauseButton.gameObject.SetActive(true);
             Pause.StartTime();
+            
         }
     }
 }
